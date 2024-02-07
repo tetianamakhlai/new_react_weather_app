5cad5b09a60aee9d4330b788469c924792906520
tetianamakhlai <tetianamakhlai@gmail.com>
2024-02-07 22:48:17 +0100

    project set

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
+
+function App(props) {
+  function showWeather(response) {
+    alert(response.data.temperature.current);
+  }
+
+  function handleEvent(event) {
+    event.preventDefault();
+    let apiKey = "03824abff0933330t40db0e3a5obfa62";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  return (
+    <div className="App">
+      <h1> Hi</h1>
+      <button onClick={handleEvent}> Api call </button>
+    </div>
+  );
+}
+
+export default App;
